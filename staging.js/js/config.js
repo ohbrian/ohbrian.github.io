af55897014ae6341097156ffbc8836d9c9506bcb
Brian Oh <oh.brian@hotmail.com>
2020-11-05 12:16:05 -0500

    Dev (#3)
    * initial drops
    
    * Update to use external css and js
    
    * Merged from Master
    
    * Update to use external css and js
    
    * update to use external JS and configs

---
+++ staging.js/js/config.js
@@ -0,0 +1,14 @@
+
+    var config = {
+        themes: [{ 
+            theme : {
+                elementId: "#canvas-theme1",
+                callers: ['#preview-theme1']
+            }
+        },{
+            theme: {
+                elementId: "#canvas-theme2",
+                callers: ['#preview-theme2']
+            }
+        }]
+    };
